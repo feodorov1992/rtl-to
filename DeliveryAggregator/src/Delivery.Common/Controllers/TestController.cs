4f0090357783a00c66d6f802bf3193692f72e6b1
Igor M <medvedkovigor@gmail.com>
2022-03-27 12:46:31 +0400

    Initial project structure

---
+++ DeliveryAggregator/src/Delivery.Common/Controllers/TestController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Delivery.Common.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class TestController
+{
+    [HttpGet]
+    public ActionResult<string> Test()
+    {
+        return "Test";
+    }
+}
