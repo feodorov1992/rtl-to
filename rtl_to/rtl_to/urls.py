6bd39d14917047e28271c6772a907a9d0becf9a1
Даниил Федоров <feodorov1992@gmail.com>
2022-05-16 19:40:28 +0300

    static pages added

--- rtl_to/rtl_to/urls.py
+++ rtl_to/rtl_to/urls.py
@@ -1,5 +1,8 @@
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+
+from rtl_to import settings
 
 urlpatterns = [
     path('', include('static_pages.urls')),
@@ -7,4 +10,4 @@ urlpatterns = [
     path('profile/', include('app_auth.urls')),
     path('management/', include('management.urls')),
     path('clientsarea/', include('clientsarea.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- rtl_to/static_pages/urls.py
+++ rtl_to/static_pages/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from static_pages.views import home_view
+from static_pages.views import home_view, requisites, price_list
 
 urlpatterns = [
     path('', home_view, name='home'),
+    path('requisites/', requisites, name='requisites'),
+    path('price/', price_list, name='price_list'),
 ]
