70a64cf7defd01a468d35552ceb8f317488a7331
Даниил Федоров <feodorov1992@gmail.com>
2022-10-19 20:15:12 +0300

    test_commit

--- rtl_to/print_forms/urls.py
+++ rtl_to/print_forms/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from print_forms.views import waybill
+from print_forms.views import waybill, waybill_page
 
 urlpatterns = [
     path('<uuid:segment_pk>/waybill/', waybill, name='waybill'),
+    path('<uuid:segment_pk>/waybill_page/', waybill_page, name='waybill_page')
 ]
