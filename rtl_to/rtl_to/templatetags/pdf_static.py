3db7943224404c38dea43b0bcd45227a228593ec
Даниил Федоров <feodorov1992@gmail.com>
2022-11-01 16:55:05 +0300

    pdf static tags updated

--- rtl_to/rtl_to/templatetags/pdf_static.py
+++ rtl_to/rtl_to/templatetags/pdf_static.py
@@ -9,7 +9,7 @@ register = template.Library()
 @register.simple_tag(takes_context=True)
 def static(context, path):
     if settings.ALLOWED_HOSTS:
-        return f'https://{settings.ALLOWED_HOSTS[0]}/{settings.STATIC_URL}{path}'
+        return f'http://{settings.ALLOWED_HOSTS[0]}/{settings.STATIC_URL}{path}'
     else:
         return f'http://127.0.0.1:8000/{settings.STATIC_URL}{path}'
 
@@ -17,6 +17,6 @@ def static(context, path):
 @register.simple_tag(takes_context=True)
 def media(context, path):
     if settings.ALLOWED_HOSTS:
-        return f'https://{settings.ALLOWED_HOSTS[0]}/{settings.MEDIA_URL}{path}'
+        return f'http://{settings.ALLOWED_HOSTS[0]}/{settings.MEDIA_URL}{path}'
     else:
         return f'http://127.0.0.1:8000/{settings.MEDIA_URL}{path}'
