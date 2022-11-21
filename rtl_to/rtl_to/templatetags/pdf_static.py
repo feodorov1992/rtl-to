5bb3315230ac1738bae160b26d1ee0aaa0e5421f
Даниил Федоров <feodorov1992@gmail.com>
2022-11-21 12:19:20 +0300

    Changed wkhtmltopdf links from external urls to local file paths

--- rtl_to/rtl_to/templatetags/pdf_static.py
+++ rtl_to/rtl_to/templatetags/pdf_static.py
@@ -1,22 +1,24 @@
 from django import template
 from django.urls import reverse
 
-from rtl_to import settings
+from django.conf import settings
 
 register = template.Library()
 
 
 @register.simple_tag(takes_context=True)
 def static(context, path):
-    if settings.ALLOWED_HOSTS:
-        return f'http://{settings.ALLOWED_HOSTS[0]}/{settings.STATIC_URL}{path}'
-    else:
-        return f'http://127.0.0.1:8000/{settings.STATIC_URL}{path}'
+    # if settings.ALLOWED_HOSTS:
+    #     return f'http://{settings.ALLOWED_HOSTS[0]}/{settings.STATIC_URL}{path}'
+    # else:
+    #     return f'http://127.0.0.1:8000/{settings.STATIC_URL}{path}'
+    return f'{settings.BASE_DIR}/static/{path}'
 
 
 @register.simple_tag(takes_context=True)
 def media(context, path):
-    if settings.ALLOWED_HOSTS:
-        return f'http://{settings.ALLOWED_HOSTS[0]}/{settings.MEDIA_URL}{path}'
-    else:
-        return f'http://127.0.0.1:8000/{settings.MEDIA_URL}{path}'
+    # if settings.ALLOWED_HOSTS:
+    #     return f'http://{settings.ALLOWED_HOSTS[0]}/{settings.MEDIA_URL}{path}'
+    # else:
+    #     return f'http://127.0.0.1:8000/{settings.MEDIA_URL}{path}'
+    return f'{settings.BASE_DIR}/media/{path}'
