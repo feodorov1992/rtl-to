4cf38d0d5e22496fb5402117a62132e0492712e2
Даниил Федоров <feodorov1992@gmail.com>
2022-12-01 18:37:24 +0300

    rm_dupl_cps management command implementation started

--- rtl_to/app_auth/management/commands/rm_dupl_cps.py
+++ rtl_to/app_auth/management/commands/rm_dupl_cps.py
@@ -24,10 +24,57 @@ class Command(BaseCommand):
                 result.pop(label)
         return result
 
+    @staticmethod
+    def collect_field(field_name, obj_list):
+        for obj in obj_list:
+            if hasattr(obj, field_name):
+                value = obj.__getattribute__(field_name)
+                if value:
+                    return value
+        else:
+            for obj in obj_list:
+                if hasattr(obj, field_name):
+                    return obj.__getattribute__(field_name)
+
+    def collect_requisites(self, obj_list: list, fields_list: list = None):
+        if fields_list is None:
+            fields_list = [
+                'inn', 'kpp', 'ogrn', 'short_name', 'full_name', 'legal_address', 'fact_address', 'client',
+                'contractor', 'admin'
+            ]
+        kwargs = dict()
+        for field in fields_list:
+            kwargs[field] = self.collect_field(field, obj_list)
+        return kwargs
+
+    @staticmethod
+    def merged_contacts(cps_list):
+        contacts_list = list()
+        for cp in cps_list:
+            for contact in cp.contacts.all():
+                if contact not in contacts_list:
+                    contacts_list.append(contact)
+        return contacts_list
+
+    def update_orders(self, cps_list: list):
+        new_cp = Counterparty.objects.create(**self.collect_requisites(cps_list))
+        new_cp.contacts.add(*self.merged_contacts(cps_list))
+        for cp in cps_list:
+            for rel_name in 'sent_transits', 'sent_ext_orders', 'sent_segments':
+                cp.__getattribute__(rel_name).all().update(sender=new_cp)
+            for rel_name in 'received_transits', 'received_ext_orders', 'received_segments':
+                cp.__getattribute__(rel_name).all().update(receiver=new_cp)
+
     def handle(self, *args, **options):
         for client in Client.objects.all():
-            print(client, len(self.collect_non_unique(client.pk, Client)))
+            non_unique = self.collect_non_unique(client.pk, Client)
+            if non_unique:
+                for _, obj_list in non_unique.items():
+                    self.update_orders(obj_list)
         print()
         for contr in Contractor.objects.all():
-            print(contr, len(self.collect_non_unique(contr.pk, Contractor)))
+            non_unique = self.collect_non_unique(contr.pk, Contractor)
+            if non_unique:
+                for _, obj_list in non_unique.items():
+                    self.update_orders(obj_list)
 
