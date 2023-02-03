b5d035e0643ce72ad86434c6f13041775c456432
DarthMole <feodorov1992@mail.ru>
2023-02-03 20:58:26 +0300

    Restructured project for dockerization

--- rtl_to/app_auth/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class AppAuthConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'app_auth'
-    verbose_name = 'Авторизация'
--- rtl_to/print_forms/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class PrintFormsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'print_forms'
