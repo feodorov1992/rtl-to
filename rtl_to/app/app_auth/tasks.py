4fb39efd89d04f3741b0cd947a03ce65214c470b
DarthMole <feodorov1992@mail.ru>
2023-08-14 13:54:01 +0300

    Added response detailed logging on exception

--- rtl_to/app/app_auth/tasks.py
+++ rtl_to/app/app_auth/tasks.py
@@ -3,6 +3,7 @@ import logging
 import requests
 from django.utils import timezone
 from django.apps import apps
+from requests.exceptions import InvalidJSONError
 
 from app_auth.mailer import ContractDepletionManagerNotification
 from rtl_to.celery import app
@@ -20,19 +21,24 @@ def contract_depletion_for_manager(contract_pk):
 @app.task
 def get_latest_currency_rates():
     url = 'https://www.cbr-xml-daily.ru/daily_json.js'
-    rates = requests.get(url, verify=False).json()
-    CurrencyRate = apps.get_model('app_auth', 'CurrencyRate')
-    CurrencyRate.objects.filter(date=timezone.now().date()).delete()
-    bulk = CurrencyRate.objects.bulk_create([
-        CurrencyRate(
-            EUR=rates['Valute']['EUR']['Value'],
-            USD=rates['Valute']['USD']['Value'],
-            GBP=rates['Valute']['GBP']['Value']
-        )
-    ])
-    obj = bulk[0]
-    obj.refresh_from_db()
-    logger.info('Rates for {} created. USD: {}; EUR: {}; GBP: {}'.format(
-        obj.date.isoformat(),
-        obj.USD, obj.EUR, obj.GBP
-    ))
+    response = requests.get(url, verify=False)
+    try:
+        rates = response.json()
+        CurrencyRate = apps.get_model('app_auth', 'CurrencyRate')
+        CurrencyRate.objects.filter(date=timezone.now().date()).delete()
+        bulk = CurrencyRate.objects.bulk_create([
+            CurrencyRate(
+                EUR=rates['Valute']['EUR']['Value'],
+                USD=rates['Valute']['USD']['Value'],
+                GBP=rates['Valute']['GBP']['Value']
+            )
+        ])
+        obj = bulk[0]
+        obj.refresh_from_db()
+        logger.info('Rates for {} created. USD: {}; EUR: {}; GBP: {}'.format(
+            obj.date.isoformat(),
+            obj.USD, obj.EUR, obj.GBP
+        ))
+    except InvalidJSONError:
+        logger.error(f'{response.url} {response.status_code} {response.reason} {response.text}')
+        raise
